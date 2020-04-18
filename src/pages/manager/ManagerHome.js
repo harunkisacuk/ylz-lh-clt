5c5c51dc3fe7c174c4ce6f6b9d06af6c945d406b
Ali <sonmez.alisonmez@gmail.com>
2020-04-19 00:06:52 +0100

    Delete button element in ManagerHome.js

--- src/pages/manager/ManagerHome.js
+++ src/pages/manager/ManagerHome.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Button from "../../atoms/Button";
 
 const ManagerHome = () => {
   return (
     <div>
       <h2>Welcome to Manager home page</h2>
-      <Button />
     </div>
   );
 };

b7917e7cad434c60f2a86b78c7e69d0961521fd3
Your Name <you@example.com>
2020-04-19 00:23:30 +0100

    bug fix

--- src/atoms/Heading.js
+++ src/atoms/Heading.js
@@ -1,8 +1,6 @@
-import React from "react";
-import { Heading } from "react-bootstrap";
+import React from 'react';
 
-export default function Heading({ size = "1", text, id }) {
-  const Title = `h${size}`;
-
-  return <Title id={id}>{text}</Title>;
+export default function Heading({ size = '1', text, id }) {
+    const Title = `h${size}`;
+    return <Title id={id}>{text}</Title>;
 }
